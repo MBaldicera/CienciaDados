8f6bf6cca7accba4d20203c4d8a055d8f510af77
MBaldicera <126473999+MBaldicera@users.noreply.github.com>
2024-10-22 12:46:39 -0300

    Add files via upload

--- graficos/qtUsuarios.js
+++ graficos/qtUsuarios.js
@@ -1,11 +1,11 @@
-import {getCSS} from "./common.js"
+import {getCSS,tickConfig} from "./common.js";
 
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json';
+    const res = await fetch(url);
+    const dados = await res.json();
+    const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados);
 
     const data = [
         {
@@ -13,21 +13,47 @@ async function quantidadeUsuariosPorRede() {
             y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
-                color: getCSS('--cor-primaria')
+                color: getCSS('--primary-color')
             }
         }
     ]
 
-    const layout = {
+    const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color')
-
+        paper_bgcolor: getCSS('--bg-color'),
+        title:{
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--cor-primaria'),
+                size: 30,
+                font: getCSS('--fonte')
+            }
+        },
+        xaxis:{
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        },
+        yaxis:{
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--cor-secundaria')
+                }
+            }
+        }
     }
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, layout)
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    Plotly.newPlot(grafico, data, laytout);
 }
 
 quantidadeUsuariosPorRede()
