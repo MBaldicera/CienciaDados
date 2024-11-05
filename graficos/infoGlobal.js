13c22e6892c749a44dee88dca06befb8bae270ce
MBaldicera <126473999+MBaldicera@users.noreply.github.com>
2024-11-05 16:47:08 -0300

    Add files via upload

--- graficos/infoGlobal.js
+++ graficos/infoGlobal.js
@@ -1,20 +1,24 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
+const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/transporte/transporte-dados-globais.json';
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url);
     const dados = await res.json();
-    const pessoasConectadas = (dados.total-conectadas/1e9)
-    const pessoasNoMundo = (dados.total-mundo/1e9)
-    const horas = parseInt(dados.tempo-medio)
-    const minutos = Math.round((dados.tempo-medio-horas)*100)
-    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo)*100).toFixed(2)
+    const trabalhadores = (dados.total_trabalhadores_mundo/1e9);
+    const pessoasTransporte = (dados.total_pessoas_que_necessitam_transporte_para_trabalho/1e9);
+    const horas = parseInt(dados.tempo_medio_deslocamento_para_trabalho);
+    const minutos = Math.round((dados.total_pessoas_mundo)*100);
 
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `O mundo tem <span>${dados.total-mundo}</span>  
+    bilhões de pessoas, dessas pessoas, aproximadamente <span>${dados.trabalhadores-mundo}</span> 
+    bilhões estão empregadas e passam em média <span>${dados.tempo-medio}</span> 
+    horas por dia trabalhando. Temos, portanto, mais da metade da população mundial que não exerce trabalhos 
+    com vínculos empregatícios legais. O que pode significar que há longevidade e desse modo, muitas pessoas aposentadas. 
+    Porém, também pode significar muitos postos de trabalho chamados informais.`
 
-    const container = document.getElementById('graficos-container')
-    container.appendChild(paragrafo)
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
 }
 
-vizualizarInformacoesGlobais()
+vizualizarInformacoesGlobais();
